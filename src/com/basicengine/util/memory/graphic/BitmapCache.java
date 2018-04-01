1d8bc29938dd599f53adef7ff7ba8582bd6a8bc8
PANJIAN <1057237562@qq.com>
2018-04-01 14:23:43 +0800

    next step:
    Debug Bitmap Cache
    Create OpenGL Frame

---
+++ src/com/basicengine/util/memory/graphic/BitmapCache.java
@@ -0,0 +1,185 @@
+package com.basicengine.util.memory.graphic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import com.basicengine.util.memory.graphic.opengl.BackGroundRendering;
+
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.opengl.GLUtils;
+
+public class BitmapCache {
+	/*This theory isn't tested yet*/
+	/*It may not taking action*/
+	/*This will save Bitmap into GPU(Follow my mind)*/
+	/*
+	Picture picture = new Picture();
+	HashMap<String, Rect> pics = new HashMap<String, Rect>();
+	Position nowPos;
+	Canvas gCanvas;
+	int direction = 0;
+	
+	Rect size;
+	
+	public BitmapCache(int Width, int Height) {
+		gCanvas = picture.beginRecording(Width, Height); // 1 can be changed(Maybe) This should be the maxium size of the picture
+		size.right = Width;
+		size.bottom = Height;
+		nowPos = new Position(Width, Height);
+	}
+	
+	public String saveBitmap(Bitmap bitmap) {
+		gCanvas.drawBitmap(bitmap, nowPos.X, nowPos.Y, null);
+		String UUID = System.currentTimeMillis() + "";
+		pics.put(UUID, new Rect(nowPos.X, nowPos.Y, nowPos.X + size.right, nowPos.Y + size.bottom));
+		if (nowPos.X < nowPos.Y) {
+			nowPos.X += size.right;
+		} else {
+			nowPos.Y += size.bottom;
+		}
+		return UUID;
+	}
+	
+	public Bitmap readBitmap(String UUID) {
+		new Pic
+	}*/
+
+	BackGroundRendering mbgr;
+	GL10 gl10;
+	final int[] GL_TEXTURE = { GL10.GL_TEXTURE0, GL10.GL_TEXTURE1, GL10.GL_TEXTURE2, GL10.GL_TEXTURE3, GL10.GL_TEXTURE4,
+	        GL10.GL_TEXTURE5, GL10.GL_TEXTURE6, GL10.GL_TEXTURE7, GL10.GL_TEXTURE8, GL10.GL_TEXTURE9, GL10.GL_TEXTURE10,
+	        GL10.GL_TEXTURE11, GL10.GL_TEXTURE12, GL10.GL_TEXTURE13, GL10.GL_TEXTURE14, GL10.GL_TEXTURE15 };
+	ArrayList<Integer> textureid = new ArrayList<Integer>();
+	int index = 0;
+	ArrayList<Stack> notfull = new ArrayList<Stack>();
+
+	public HashMap<String, Content> bmp = new HashMap<String, Content>();
+	int size = 512;
+
+	public BitmapCache(int s) {
+		size = s;
+		mbgr = new BackGroundRendering(size);
+		gl10 = mbgr.gl10;
+	}
+
+	public BitmapCache() {
+		mbgr = new BackGroundRendering();
+		gl10 = mbgr.gl10;
+	}
+
+	private void addBitmapToTexture(int index, Bitmap bitmap, Position where) {
+		gl10.glClearColor(0f, 0f, 0f, 0f);
+		gl10.glTranslatef(0, 0, 0.0f);
+		gl10.glBindTexture(GL_TEXTURE[index], textureid.get(index));
+		gl10.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4); // get olds pic
+
+		gl10.glEnable(GL10.GL_TEXTURE_2D);
+		int[] textures = new int[1];
+		gl10.glGenTextures(1, textures, 0);
+		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+		gl10.glTranslatef(where.X, where.Y, 0.0f); //Draw picture into specific place
+		gl10.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+		gl10.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);// drawing new pics
+
+		Bitmap btm = mbgr.getContent(0, 0, size, size); // get mixed pic
+		int[] newtexture = new int[1];
+		gl10.glGenTextures(1, newtexture, 0);
+		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, btm, 0);// update into texture
+		textureid.set(index, newtexture[0]);
+	}
+
+	public String addBitmap(Bitmap bitmap, boolean recycle) {
+		for (Stack s : notfull.toArray(new Stack[0])) {
+			if (size - s.filled.right - bitmap.getWidth() >= 0) {
+				if (size - s.filled.top - bitmap.getHeight() >= 0) { // out of range
+					if (bitmap.getHeight() + s.filled.top > s.filled.bottom) { // a longer picture
+						s.filled.bottom = bitmap.getHeight() + s.filled.top;
+					}
+					// allocate texture
+					addBitmapToTexture(s.index, bitmap, new Position(s.filled.top, s.filled.left));
+					if (recycle) {
+						bitmap.recycle();
+					}
+					//assign ID and Rect
+					String UUID = System.currentTimeMillis() + "";
+					bmp.put(UUID, new Content(new Rect(s.filled.left, s.filled.top, s.filled.left + bitmap.getWidth(),
+					        s.filled.top + bitmap.getHeight()), s.index));
+					s.filled.left += bitmap.getWidth();
+					return UUID;
+				}
+			} else { // a fat picture ('◡')
+				s.filled.top = s.filled.bottom;
+				s.filled.left = 0; // next line
+				addBitmapToTexture(s.index, bitmap, new Position(s.filled.top, s.filled.left));
+				if (recycle) {
+					bitmap.recycle();
+				}
+
+				String UUID = System.currentTimeMillis() + "";
+				bmp.put(UUID, new Content(new Rect(s.filled.left, s.filled.top, s.filled.left + bitmap.getWidth(),
+				        s.filled.top + bitmap.getHeight()), s.index));
+				s.filled.left += bitmap.getWidth();
+				return UUID;
+			}
+		}
+		gl10.glEnable(GL_TEXTURE[index]); // create new texture
+		int[] textures = new int[1];
+		gl10.glGenTextures(1, textures, 0);
+		GLUtils.texImage2D(GL_TEXTURE[index], 0, bitmap, 0);
+		textureid.add(textures[0]);
+		if (recycle) {
+			bitmap.recycle();
+		}
+
+		String UUID = System.currentTimeMillis() + ""; // allocate IDs
+		bmp.put(UUID, new Content(new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight()), index));
+		if (bitmap.getWidth() < size || bitmap.getHeight() < size) {
+			notfull.add(new Stack(index, new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight())));
+		}
+		index++;
+		return UUID;
+	}
+
+	public Bitmap getBitmap(String UUID) {
+		Content c = bmp.get(UUID);
+		gl10.glClearColor(0, 0, 0, 0);
+		gl10.glTranslatef(0, 0, 0.0f);
+		gl10.glBindTexture(GL_TEXTURE[c.index], textureid.get(c.index)); // draw texture into drawer
+		gl10.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+		return mbgr.getContent(c.rect.left, c.rect.top, c.rect.right - c.rect.left, c.rect.bottom - c.rect.top); // get allocated area's image content
+	}
+
+}
+
+class Position {
+	int X;
+	int Y;
+
+	public Position(int x, int y) {
+		X = x;
+		Y = y;
+	}
+}
+
+class Content {
+	Rect rect;
+	int index;
+
+	public Content(Rect r, int i) {
+		rect = r;
+		index = i;
+	}
+}
+
+class Stack {
+	public Rect filled = new Rect(0, 0, 0, 0);
+	public int index;
+
+	public Stack(int i, Rect r) {
+		index = i;
+		filled = r;
+	}
+}
---
+++ src/com/basicengine/util/memory/graphic/opengl/BackGroundRendering.java
@@ -0,0 +1,96 @@
+package com.basicengine.util.memory.graphic.opengl;
+
+import java.nio.IntBuffer;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+public class BackGroundRendering {
+
+	public EGL10 mEgl;
+	private int[] version = new int[2];
+	EGLConfig[] configs = new EGLConfig[1];
+	int[] num_config = new int[1];
+	int[] configSpec = { EGL10.EGL_SURFACE_TYPE, EGL10.EGL_PBUFFER_BIT, EGL10.EGL_RED_SIZE, 8, EGL10.EGL_GREEN_SIZE, 8,
+	        EGL10.EGL_BLUE_SIZE, 8, EGL10.EGL_ALPHA_SIZE, 8, EGL10.EGL_NONE };
+	int attribListPbuffer[] = { EGL10.EGL_WIDTH, 512, EGL10.EGL_HEIGHT, 512, EGL10.EGL_NONE };
+
+	public GL10 gl10;
+	public int size;
+	
+	public BackGroundRendering() {
+		initEGL();
+	}
+
+	public BackGroundRendering(int s) {
+		size = s;
+		attribListPbuffer = new int[5];
+		attribListPbuffer[0] = EGL10.EGL_WIDTH;
+		attribListPbuffer[1] = attribListPbuffer[3] = s;
+		attribListPbuffer[2] = EGL10.EGL_HEIGHT;
+		attribListPbuffer[4] = EGL10.EGL_NONE;
+		initEGL();
+	}
+
+	private void initEGL() {
+		mEgl = (EGL10) EGLContext.getEGL();
+		EGLDisplay mEglDisplay = mEgl.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+		mEgl.eglInitialize(mEglDisplay, version);
+		mEgl.eglChooseConfig(mEglDisplay, configSpec, configs, 1, num_config);
+		EGLConfig mEglConfig = configs[0]; // creating background drawing unit
+		EGLContext mEglContext = mEgl.eglCreateContext(mEglDisplay, mEglConfig, EGL10.EGL_NO_CONTEXT, null);
+		if (mEglContext == EGL10.EGL_NO_CONTEXT) {
+			Log.d("ERROR:", "EGL_NO_CONTEXT");
+		}
+		EGLSurface mEglPBSurface = mEgl.eglCreatePbufferSurface(mEglDisplay, mEglConfig, attribListPbuffer);
+		if (mEglPBSurface == EGL10.EGL_NO_SURFACE) {
+			int ec = mEgl.eglGetError();
+			if (ec == EGL10.EGL_BAD_DISPLAY) {
+				Log.d("ERROR:", "EGL_BAD_DISPLAY");
+			}
+			if (ec == EGL10.EGL_BAD_DISPLAY) {
+				Log.d("ERROR:", "EGL_BAD_DISPLAY");
+			}
+			if (ec == EGL10.EGL_NOT_INITIALIZED) {
+				Log.d("ERROR:", "EGL_NOT_INITIALIZED");
+			}
+			if (ec == EGL10.EGL_BAD_CONFIG) {
+				Log.d("ERROR:", "EGL_BAD_CONFIG");
+			}
+			if (ec == EGL10.EGL_BAD_ATTRIBUTE) {
+				Log.d("ERROR:", "EGL_BAD_ATTRIBUTE");
+			}
+			if (ec == EGL10.EGL_BAD_ALLOC) {
+				Log.d("ERROR:", "EGL_BAD_ALLOC");
+			}
+			if (ec == EGL10.EGL_BAD_MATCH) {
+				Log.d("ERROR:", "EGL_BAD_MATCH");
+			}
+		}
+		if (!mEgl.eglMakeCurrent(mEglDisplay, mEglPBSurface, mEglPBSurface, mEglContext))//这里mEglPBSurface，意思是画图和读图都是从mEglPbSurface开始
+		{
+			Log.d("ERROR:", "bind failed ECODE:" + mEgl.eglGetError());
+		}
+		gl10 = (GL10) mEglContext.getGL();
+	}
+
+	public Bitmap getContent(int x, int y, int width, int height) { // get bitmap from
+		IntBuffer PixelBuffer = IntBuffer.allocate(width * height);
+		PixelBuffer.position(0);
+		gl10.glReadPixels(x, y, width, height, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, PixelBuffer);
+		PixelBuffer.position(0);
+		int pix[] = new int[width * height];
+		PixelBuffer.get(pix);
+
+		Bitmap bmp = Bitmap.createBitmap(pix, width, height, Bitmap.Config.ARGB_8888);
+		return bmp;
+	}
+
+}
