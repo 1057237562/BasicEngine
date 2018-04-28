9b378e332a2437f3b443f0d65cce5b80958b0b9f
PANJIAN <1057237562@outlook.com>
2018-04-28 17:50:12 +0800

    s7

--- src/com/basicengine/graphics/opengl/Grid.java
+++ src/com/basicengine/graphics/opengl/Grid.java
@@ -2,23 +2,47 @@ package com.basicengine.graphics.opengl;
 
 import java.util.ArrayList;
 
+import javax.microedition.khronos.opengles.GL10;
+
+import com.basicengine.util.memory.graphic.opengl.BackGroundRendering;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+
 public class Grid extends GLGUI {
 
 	public ArrayList<GLRenderObject> objects = new ArrayList<GLRenderObject>();
+	BackGroundRendering bgr = new BackGroundRendering();
+	GLBitmap content;
 
 	public Grid(GLBitmap texture, int X, int Y, int width, int height) {
 		super(texture, X, Y, width, height);
+		content = new GLBitmap(parent.gl10, Bitmap.createBitmap(width, height, Config.ARGB_8888));
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-		for (int i = objects.size() - 1; i >= 0; i--) {
-			objects.get(i).draw();
-		}
+		content.draw();
 		super.draw();
 	} // Take care of the extends relationship
 	  // Reset Original Point of the items
+
+	public void addRenderObject(GLRenderObject object) {
+		objects.add(object);
+	}
+	
+	public GL10 getGridGL() {
+		return bgr.gl10;
+	}
+
+	public void RegenerateTexture() {
+		for (int i = objects.size() - 1; i >= 0; i--) {
+			objects.get(i).draw();
+		}
+
+		content.loadGLTexture(bgr.getContent(0, 0, rect.right - rect.left, rect.bottom - rect.top));
+	}
 
 }
